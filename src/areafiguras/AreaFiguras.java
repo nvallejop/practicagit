16ac8e8d841620cfbd882ed1971903241ceaf11f
Elias <eliasreyeselias@gmail.com>
2019-08-21 13:08:49 -0500

    nueva opcion de area de circulo

--- src/areafiguras/AreaFiguras.java
+++ src/areafiguras/AreaFiguras.java
@@ -33,6 +33,11 @@ public class AreaFiguras {
 					altura=solicitarValor("Indica la altura: ");
 					System.out.println("\nEl area del rectangulo es " + (base*altura));
 					break;
+                                case 3:
+					// area de un circulo
+					radio=solicitarValor("Indica el radio: ");
+					System.out.printf("\nEl area de la circunferencia es %1.2f\n", Math.PI*(Math.pow(radio,2)));
+					break;
     
 }
                 }
